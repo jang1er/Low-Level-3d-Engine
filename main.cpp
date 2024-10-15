951ca9b1e3519a8bb91f1e62df368888b21f2d14
jonas kruger <jonas.kruger@uni-ulm.de>
2024-10-15 17:50:44 +0200

    feat(*): implemented 3d MVP Matrix on previous triangle

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <common/shader.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 using namespace glm;
 
@@ -13,13 +14,17 @@ int main() {
         return -1;
     }
 
+    constexpr int WINDOW_WIDTH = 1024;
+    constexpr int WINDOW_HEIGHT = 768;
+    constexpr float ASPECT_RATIO = (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT;
+
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
     glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
     glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE ); // make MaOs happy
     glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 
-    GLFWwindow* window = glfwCreateWindow(1024 , 768, "Low Level 3d Engine", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH , WINDOW_HEIGHT, "Low Level 3d Engine", NULL, NULL);
     if(window == NULL) {
         fprintf( stderr, "Failed to open GLFW window \n" );
         glfwTerminate();
@@ -55,7 +60,28 @@ int main() {
 
     GLuint programID = LoadShaders("src/shaders/SimpleShader.vert", "src/shaders/SimpleShader.frag");
 
+    // get the MVP ID to be used for the shader
+    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+    // Create the projection Matrix: 45° FOV, Aspect Ratio: Width / Height, display range: 0.1 -> 100.0 units
+    mat4 Projection = glm::perspective(glm::radians(45.0f), ASPECT_RATIO, 0.1f, 100.0f);
+
+    mat4 View = glm::lookAt(
+        vec3(4,3,3),            // camera is at (4,3,3)
+        vec3(0, 0, 0),          // looks ath the world origin
+        vec3(0, 1, 0)           // and the head is up
+    );
+
+    // model matrix: scaling with a factor of (2, 1.5f, -1)
+    mat4 Model = glm::scale(glm::mat4(1), vec3(2,1.5f,-1));
+
+    // construct the project view model matrix
+    mat4 MVP = Projection * View * Model;
+
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    // set background
+    glClearColor(0.2f, 0.0f, 0.7f, 1.0f);
     do {
         // Clear the screen
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -74,6 +100,9 @@ int main() {
 
         // use our shader
         glUseProgram(programID);
+
+        // give the shader the MVP matrix
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
         // Draw the triangle
         glDrawArrays(GL_TRIANGLES, 0, 3);
--- src/shaders/SimpleShader.vert
+++ src/shaders/SimpleShader.vert
@@ -1,7 +1,11 @@
 #version 330 core
+// vertex input data
 layout(location = 0) in vec3 vertexPosition_modelspace;
 
+// Model View Projection Matrix
+uniform mat4 MVP;
+
 void main(){
-    gl_Position.xyz = vertexPosition_modelspace;
-    gl_Position.w = 1.0;
+    // final position for the vertex: MVP * position
+    gl_Position = MVP * vec4(vertexPosition_modelspace,1);
 }
